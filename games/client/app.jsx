2a668c10e165965178e79be91e1a3cad33b45b69
Daniela Howe <daniela.h@Danielas-MacBook-Pro-2.local>
2016-02-10 10:43:19 -0800

    Add image

--- games/client/app.jsx
+++ games/client/app.jsx
@@ -1,29 +1,9 @@
 // App component - represents the whole app
 App = React.createClass({
-  getTasks() {
-    return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
-    ];
-  },
- 
-  renderTasks() {
-    return this.getTasks().map((task) => {
-      return <Task key={task._id} task={task} />;
-    });
-  },
- 
   render() {
-    return (
+    return(
       <div className="container">
-        <header>
-          <h1>Todo List</h1>
-        </header>
- 
-        <ul>
-          {this.renderTasks()}
-        </ul>
+        <p>test</p>
       </div>
     );
   }
--- games/client/simple-todos-react.jsx
+++ games/client/simple-todos-react.jsx
@@ -3,6 +3,6 @@ if (Meteor.isClient) {
  
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
-    React.render(<App />, document.getElementById("render-target"));
+    React.render(<Background />, document.getElementById("render-target"));
   });
 }
